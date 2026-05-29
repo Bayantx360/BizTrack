2b531d150420ca028b66b6b086bed4751ed45850
Abdul-WriteCodes <145736001+Abdul-WriteCodes@users.noreply.github.com>
2026-05-29 02:32:07 +0100

    Update suite_home.py

--- suite_home.py
+++ suite_home.py
@@ -140,8 +140,8 @@ def page_login():
     if biz_count > 0:
         avatar_colors = [
             ("E8F4FD", "1A6FA8"),
-            ("E6FFFA", "1A7A4A"),
-            ("ECFEFF", "A07A10"),
+            ("10B981", "1A7A4A"),
+            ("164E63", "A07A10"),
             ("FDEDEC", "A83228"),
         ]
         avatars_html = ""
